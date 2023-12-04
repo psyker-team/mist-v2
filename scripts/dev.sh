c74683489b3eec0728b365fa747052bddfe97e1d
CaradryanLiang <caradryan2022@gmail.com>
2023-12-04 11:51:03 -0800

    fixing fp16

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -58,3 +58,23 @@ accelerate launch attacks/ita.py \
  --original_resolution
 
 
+  accelerate launch attacks/ita_diffusers_version.py \
+ --model_type sd \
+ --cuda \
+ --low_vram_mode \
+ --pretrained_model_name_or_path stable-diffusion/stable-diffusion-1-5/ \
+ --instance_data_dir data/training/kent \
+ --output_dir output/mist/sd1-5/kent \
+ --instance_prompt "a photo of a animate person in the style of sks, high quality, masterpiece" \
+ --gradient_checkpointing \
+ --low_vram_mode \
+ --mixed_precision bf16 \
+ --max_f_train_steps 5 \
+ --max_train_steps 50 \
+ --num_train_epochs 1 \
+ --max_adv_train_steps 50 \
+ --pgd_eps 0.04 \
+ --original_resolution
+
+
+
