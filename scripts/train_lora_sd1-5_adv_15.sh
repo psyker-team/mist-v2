017e42365d6f6307c375ec5b6a1d32e2e3d2d4eb
CaradryanLiang <caradryan2022@gmail.com>
2023-12-07 20:49:45 -0800

    release

--- scripts/train_lora_sd1-5_adv_15.sh
+++ scripts/train_lora_sd1-5_adv_15.sh
@@ -1,6 +1,6 @@
 export MODEL_NAME="stable-diffusion/stable-diffusion-1-5"
-export INSTANCE_DIR="output/mist/sd1-5/painting"
-export OUTPUT_DIR="output/lora/sd1-5_adv/painting"
+export INSTANCE_DIR="output/mist/sd1-5/kent"
+export OUTPUT_DIR="output/lora/sd1-5_adv/kent"
 export CLASS_DIR="data/lora_class"
 
 accelerate launch eval/train_dreambooth_lora_15.py \
@@ -8,8 +8,8 @@ accelerate launch eval/train_dreambooth_lora_15.py \
   --instance_data_dir=$INSTANCE_DIR \
   --output_dir=$OUTPUT_DIR \
   --class_data_dir=$CLASS_DIR\
-  --instance_prompt "a painting of a sks person, high quality, masterpiece" \
- --class_prompt "a painting of a person, high quality, masterpiece" \
+  --instance_prompt "an animated painting of a sks person, high quality, masterpiece" \
+ --class_prompt "an animated painting of a person, high quality, masterpiece" \
   --resolution=512 \
   --train_batch_size=1 \
   --gradient_accumulation_steps=1 \
