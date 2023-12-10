59ef2fecfdff8a18e997aa8ed5af738ab1801ce7
CaradryanLiang <caradryan2022@gmail.com>
2023-12-10 00:28:44 -0800

    fixing max_f_train_steps

--- mist-webui.py
+++ mist-webui.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
                                       info="Training epochs of Mist-V2")
                         max_f_train_steps = gr.Slider(0, 30, step=1, value=5, label='LoRA Steps',
                                       info="Training steps of LoRA in one epoch")
-                        max_adv_train_steps = gr.Slider(0, 200, step=10, value=30, label='Attacking Steps',
+                        max_adv_train_steps = gr.Slider(0, 100, step=5, value=20, label='Attacking Steps',
                                       info="Training steps of attacking in one epoch")
                         lora_lr = gr.Number(minimum=0.0, maximum=1.0, label="The learning rate of LoRA", value=0.0001, float=True)
                         pgd_lr = gr.Number(minimum=0.0, maximum=1.0, label="The learning rate of PGD", value=0.005, float=True)
